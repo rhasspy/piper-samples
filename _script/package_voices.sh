bd8c85add57ee324274abae937774bff45f6f9a9
Michael Hansen <mike@rhasspy.org>
2023-04-15 22:38:54 -0500

    Add package voices script

---
+++ _script/package_voices.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+this_dir="$( cd "$( dirname "$0" )" && pwd )"
+voices_dir="$(realpath "${this_dir}/..")"
+
+mkdir -p "${voices_dir}/_dist"
+
+find "${voices_dir}" -name '*.onnx' | \
+    while read -r model; do
+        model_name="$(basename "${model}" .onnx)";
+        model_dir="$(dirname "${model}")";
+        if [[ -f "${model_dir}/MODEL_CARD" ]]; then
+            echo "${model_dir}";
+            cd "${model_dir}" && \
+                tar -czvf "${voices_dir}/_dist/voice-${model_name}.tar.gz" \
+                "${model_name}.onnx" \
+                "${model_name}.onnx.json" \
+                'MODEL_CARD';
+        fi
+    done
